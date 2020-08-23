129c2285626ecb7b257c5765213d4e3ec9fb6de8
Andrew Fay <andrew.fay@hotmail.co.uk>
2020-08-23 15:38:07 +0100

    added return statement function

--- helloworld.py
+++ helloworld.py
@@ -7,12 +7,12 @@ print("hello world")
 def printme (str): 
     "this prints a string you pass to it"
     print(str)
-    return
+
 
 # function
-def printme2 (str): 
-    print(str)
-    return
+def printme2 (sometext): 
+    print(sometext)
+
 
 # call function
 printme("test")
@@ -31,5 +31,27 @@ class GradeBook:
 
 grade1 = GradeBook("Emily", "A+")
 grade1.gradefunction()
+
+
+
+# Python program to  
+# demonstrate return statement  
+  
+def add(a, b): 
+  
+    # returning sum of a and b 
+    return a + b 
+  
+def is_true(a): 
+  
+    # returning boolean of a 
+    return bool(a) 
+  
+# calling function 
+res = add(2, 3) 
+print("Result of add function is {}".format(res)) 
+  
+res = is_true(2<5) 
+print("\nResult of is_true function is {}".format(res)) 
 
 
