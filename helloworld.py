7add2d04d92889012fb414cd5b3289ac28c0140c
Andrew Fay <andrew.fay@hotmail.co.uk>
2020-08-23 15:14:34 +0100

    updated helloworld file with examples

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,20 @@
+# simple print statement
 print("hello world")
 
+
+
+# function
+def printme (str): 
+    "this prints a string you pass to it"
+    print(str)
+    return
+
+# function
+def printme2 (str): 
+    print(str)
+    return
+
+# call function
+printme("test")
+
+printme2("test2")
